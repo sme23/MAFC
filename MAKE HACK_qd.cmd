3c1f5db136a53fdc1627c99e259f55570b426b6d
Sme <esahig15789@gmail.com>
2019-05-02 16:14:25 -0400

    makehack scripts

---
+++ MAKE HACK_qd.cmd
@@ -0,0 +1,14 @@
+cd %~dp0
+
+copy FE8_clean.gba _MAFC.gba
+
+cd "%~dp0Event Assembler"
+
+Core A FE8 -output:"%~dp0_MAFC.gba" -input:"%~dp0Debug.event"
+
+if exist "%~dp0ups/ups.exe" (
+    cd "%~dp0ups"
+    ups diff -b "%~dp0FE8_clean.gba" -m "%~dp0_MAFC.gba" -o "%~dp0_MAFC.ups"
+)
+
+pause
